97a8583ac10252059ef0a9966e92c2833aaaccac
Leonardo Batista Carias <leonardo1carias@gmail.com>
2021-08-01 20:07:08 -0300

    Defining Interface implementing first tests

---
+++ application/product_test.go
@@ -0,0 +1,53 @@
+package application_test
+
+import (
+	"github/LeonardoBatistaCarias/go-hexagonal/application"
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+}
+
+func TestProduct_Disabled(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "Invalid"
+	_, err = product.IsValid()
+	require.Equal(t, "The status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "The price must be greater or equal zero", err.Error())
+}
